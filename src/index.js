5ba019dd8f80d13ead2f923d5184eb3f52e00156
anna-zau <104685927+anna-zau@users.noreply.github.com>
2023-01-27 23:18:15 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio-test">
+    <BrowserRouter basename="/test-res">
       <App />
     </BrowserRouter>
   </React.StrictMode>
